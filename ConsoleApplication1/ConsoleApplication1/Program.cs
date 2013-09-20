ed493607d6a3f35b0d8f23d02e96ef33318abcd9
carlosfloresp <carlosalberto_florespresas@hotmail.com>
2013-09-19 22:25:54 -0500

    tarea

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public string nom;
+        public int ed;
+        public string cor;
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Console.WriteLine("Escribe tu nombre");
+            p.nom = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu edad");
+            p.ed = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu correo");
+            p.cor = Console.ReadLine();
+
+            Console.WriteLine("tus datos son los siguientes:\n" + "Nombre:" + p.nom + "\n" + "Edad:" + p.ed + " años\n" + "Correo:" + p.cor);
+            Console.Read();
+        }
+    }
+}
